2fd6dccdfd16c1ed3539778671bc4c6e601937a7
Hamish Moffatt <hamish@moffatt.id.au>
2018-12-05 12:36:05 +1100

    communicate with holman over socket

--- plugins/holman_control.py
+++ plugins/holman_control.py
@@ -5,14 +5,18 @@ from blinker import signal
 import subprocess
 import web, json, time
 from threading import Thread
+import socket
 import gv  # Get access to SIP's settings, gv = global variables
 from urls import urls  # Get access to SIP's URLs
 from sip import template_render
 from webpages import ProtectedPage
 
+
 gv.use_gpio_pins = False  # Signal SIP to not use GPIO pins
 
 json_data = './data/holman_control.json'
+
+holman_socket = '/run/holman.sock'
 
 # Add a new url to open the data entry page.
 urls.extend(['/holman', 'plugins.holman_control.settings',
@@ -21,7 +25,6 @@ urls.extend(['/holman', 'plugins.holman_control.settings',
 
 # Add this plugin to the plugins menu
 gv.plugin_menu.append(['Holman Timer Control', '/holman'])
-
 
 class HolmanController(Thread):
 
@@ -35,6 +38,13 @@ class HolmanController(Thread):
 
         zones = signal('zone_change')
         zones.connect(self.on_zone_change)
+
+        self.sock = socket.socket(socket.AF_UNIX, socket.SOCK_DGRAM)
+
+        # Establish initial state
+        for station, value in enumerate(self.gv.srvals):
+            if self.config['mac'][station]:
+                self.control_timer(station, self.config['runtime'] if value else 0)
 
         self.start()
 
@@ -61,6 +71,16 @@ class HolmanController(Thread):
             with open(json_data, 'w') as f:
                 json.dump(self.config, f, indent=4)
 
+    def control_timer(self, station, runtime):
+        message = json.dumps({
+            "mac": self.config['mac'][station],
+            "runtime": runtime,
+        })
+
+        try:
+            self.sock.sendto(message, holman_socket)
+        except socket.error:
+            print('failed to communicate with holman socket')
 
     def on_zone_change(self, name, **kw):
         """ Switch relays when core program signals a change in station state."""
@@ -69,14 +89,17 @@ class HolmanController(Thread):
                 if self.gv.srvals[i] != self.prior[i]: #  this station has changed
                     if self.gv.srvals[i]: # station is on
                         print('switching on station %d mac %s' % (i, self.config['mac'][i]))
+                        self.control_timer(i, self.config['runtime'])
                     else:
                         print('switching off station %d mac %s' % (i, self.config['mac'][i]))
+                        self.control_timer(i, 0)
 
             self.prior = self.gv.srvals[:]
 
     def run(self):
         while True:
             time.sleep(1)
+            # check if timer needs to be renewed
 
 
 controller = HolmanController(gv)
@@ -114,7 +137,7 @@ class update(ProtectedPage):
         for i in range(gv.sd['nst']):
             config['mac'].append(qdict['mac'+str(i)])
         config['runtime'] = int(qdict['runtime'])
-        	
+
         with open(json_data, 'w') as f:  # write the settings to file
           	json.dump(config, f, indent=4)
         raise web.seeother('/')
